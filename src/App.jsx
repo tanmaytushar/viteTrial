1293edbcaab72630426e013ea599c54e7cc07e64
Tanmay <ttushar176@gmail.com>
2024-06-04 08:36:07 +0530

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import './App.css'
+import { useState } from 'react'
+
 
 function App() {
-  
+  const [count, setCount] = useState(0)
+  function function1(){
+    setCount((count)=>count+1);
+    setCount((count)=>count+1);
+  }
   return (
     <div>
-      hi there
+      <button onClick={function1}> Count is {count} </button> 
     </div>
   )
 }
